e4ccb98974bd16cbf48fcfc449ede69a6c3895ae
OsoCoco <61520900+OsoCoco@users.noreply.github.com>
2020-10-06 23:26:20 -0500

    Primeras Pantallas

---
+++ Proyecto Final/Assets/Scripts/ActivateMenus.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateMenus : MonoBehaviour
+{
+    public void OnAction(GameObject act)
+    {
+        if (act.activeSelf)
+            act.SetActive(false);
+        else
+            act.SetActive(true);
+    }
+}
