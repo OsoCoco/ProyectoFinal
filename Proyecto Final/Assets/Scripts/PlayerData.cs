e08555a1bc4e4e0c2ace763e42e42c3dc6aa5208
OsoCoco <61520900+OsoCoco@users.noreply.github.com>
2020-10-13 12:16:58 -0500

    JSON

---
+++ Proyecto Final/Assets/Scripts/PlayerData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PlayerData : MonoBehaviour
+{
+    public string playerName;
+    public int lives;
+    public float health;
+
+    private void Start()
+    {
+       Debug.Log(SaveToString());
+    }
+    public string SaveToString()
+    {
+        return JsonUtility.ToJson(this);
+    }
+}
